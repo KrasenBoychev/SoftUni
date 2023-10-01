a2e2330d3196305996d479f5e27302681b5acb51
Krasen Boychev <krasenboychev11@gmail.com>
2023-10-01 16:10:07 +0100

    Created loading_bar.js

---
+++ Functions/loading_bar.js
@@ -0,0 +1,21 @@
+function loadingBar(inputNum){
+    
+    if(inputNum < 100){
+        console.log(`${inputNum}% [${bar(inputNum)}]`);
+        console.log(`Still loading...`);
+    } else{
+        console.log(`100% Complete!`);
+    }
+
+    function bar(inputNum){
+        let newArr = [];
+        for(let i = 0; i < (inputNum / 10); i++){
+            newArr.push("%");
+        }
+        for(let i = (inputNum / 10); i < 10; i++){
+            newArr.push(".");
+        }
+        return newArr.join("");
+    }
+}
+loadingBar(100);
