79db1bb38ba64207621e56e6c81ef60add7f84a6
Krasen Boychev <krasenboychev11@gmail.com>
2023-10-01 11:56:53 +0100

    Created 2 new files

---
+++ Functions/add_and_subract.js
@@ -0,0 +1,10 @@
+function subract(x, y, z) {
+
+    let sum = (x, y) => x + y;
+
+    let result = sum(x, y) - z;
+    console.log(result);
+}
+subract(23,
+    6,
+    10);
---
+++ Functions/smallest_of_three_numbers.js
@@ -0,0 +1,6 @@
+function smallestNumber(x, y, z) {
+    console.log(Math.min(x, y, z));
+}
+smallestNumber(600,
+    342,
+    123);
