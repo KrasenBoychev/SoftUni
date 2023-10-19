fa49eb835179d479a4657a0669b61ee2302051be
Krasen Boychev <krasenboychev11@gmail.com>
2023-10-19 16:25:51 +0100

    Created convert_to_json.js

---
+++ Objects/convert_to_json.js
@@ -0,0 +1,10 @@
+function convertToJson(name, lastName, hairColor) {
+    let person = {
+        name,
+        lastName,
+        hairColor
+    };
+    console.log(JSON.stringify(person));
+}
+convertToJson('George', 'Jones',
+'Brown');
