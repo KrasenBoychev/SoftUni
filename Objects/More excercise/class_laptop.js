afe375190e5e979299745d377145fe32cce22240
Krasen Boychev <krasenboychev11@gmail.com>
2023-10-23 15:28:34 +0100

    Created class_laptop.js

---
+++ Objects/More excercise/class_laptop.js
@@ -0,0 +1,34 @@
+class Laptop {
+    constructor(info, quality) {
+        this.info = info;
+        this.quality = quality;
+        this.isOn = false;
+    }
+
+    turnOn() {
+        this.isOn = true;
+    }
+    
+    turnOff() {
+        this.isOn = false;
+    }
+
+    showInfo() {
+        return JSON.stringify(this.info);
+    }
+
+
+
+}
+
+
+let info = {producer: "Dell", 
+age: 2, brand: "XPS"}
+let laptop = new Laptop(info, 10)
+laptop.turnOn()
+console.log(laptop.showInfo())
+// laptop.turnOff()
+// console.log(laptop.quality)
+// laptop.turnOn()
+// console.log(laptop.isOn)
+// console.log(laptop.price)
