5cfd51930441d18df9b88226a4d20822928566b8
Krasen Boychev <krasenboychev11@gmail.com>
2023-11-21 14:00:44 +0000

    changed file

--- FinalExamFundamentals/03_RetakeExam/need_for_speed(1 mistake in Judge).js
+++ FinalExamFundamentals/03_RetakeExam/need_for_speed(1 mistake in Judge).js
@@ -98,29 +98,29 @@ function needForSpeed(input) {
         console.log(`${el.car} -> Mileage: ${el.mileage} kms, Fuel in the tank: ${el.fuel} lt.`);
     }
 }
-needForSpeed([
-    '3',
-    'Audi A6|38000|62',
-    'Mercedes CLS|11000|35',
-    'Volkswagen Passat CC|45678|5',
-    'Drive : Audi A6 : 543000 : 47',
-    'Drive : Mercedes CLS : 940000 : 11',
-    'Drive : Volkswagen Passat CC : 690000 : 1',
-    'Refuel : Audi A6 : 50',
-    'Revert : Mercedes CLS : 500',
-    'Revert : Audi A6 : 30000',
-    'Stop']);
-
 // needForSpeed([
-//     '4',
-//     'Lamborghini Veneno|11111|74',
-//     'Bugatti Veyron|12345|67',
-//     'Koenigsegg CCXR|67890|12',
-//     'Aston Martin Valkryie|99900|50',
-//     'Drive : Koenigsegg CCXR : 382 : 82',
-//     'Drive : Aston Martin Valkryie : 99 : 23',
-//     'Drive : Aston Martin Valkryie : 2 : 1',
-//     'Refuel : Lamborghini Veneno : 40',
-//     'Revert : Bugatti Veyron : 2000',
-//     'Stop'
-//   ]);
+//     '3',
+//     'Audi A6|38000|62',
+//     'Mercedes CLS|11000|35',
+//     'Volkswagen Passat CC|45678|5',
+//     'Drive : Audi A6 : 543000 : 47',
+//     'Drive : Mercedes CLS : 940000 : 11',
+//     'Drive : Volkswagen Passat CC : 690000 : 1',
+//     'Refuel : Audi A6 : 50',
+//     'Revert : Mercedes CLS : 500',
+//     'Revert : Audi A6 : 30000',
+//     'Stop']);
+
+needForSpeed([
+    '4',
+    'Lamborghini Veneno|11111|74',
+    'Bugatti Veyron|12345|67',
+    'Koenigsegg CCXR|67890|12',
+    'Aston Martin Valkryie|99900|50',
+    'Drive : Koenigsegg CCXR : 382 : 82',
+    'Drive : Aston Martin Valkryie : 99 : 23',
+    'Drive : Aston Martin Valkryie : 2 : 1',
+    'Refuel : Lamborghini Veneno : 40',
+    'Revert : Bugatti Veyron : 2000',
+    'Stop'
+  ]);
