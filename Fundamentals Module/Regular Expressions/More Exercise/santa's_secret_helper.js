2b0c4c20b85515a235f3d02e552eb46c0db20b5d
Krasen Boychev <krasenboychev11@gmail.com>
2023-12-09 20:31:24 +0000

    Created santa's_secret_helper.js

---
+++ Fundamentals Module/Regular Expressions/More Exercise/santa's_secret_helper.js
@@ -0,0 +1,16 @@
+function santaSecretHelper(input) {
+   
+}
+
+santaSecretHelper(['3',
+'CNdwhamigyenumje$J$',
+'CEreelh-nmguuejnW$J$',
+'CVwdq&gnmjkvng$Q$',
+'end']);
+
+// santaSecretHelper(['3',
+// "N}eideidmk$'(mnyenmCNlpamnin$J$",
+// 'ddddkkkkmvkvmCFrqqru-nvevek$J$nmgievnge',
+// 'ppqmkkkmnolmnnCEhq/vkievk$Q$',
+// 'yyegiivoguCYdohqwlqh/kguimhk$J$',
+// 'end']);
