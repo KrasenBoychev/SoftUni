86ee9b2eecc0aee36dec2814a9ded3e1ab47403d
Krasen Boychev <krasenboychev11@gmail.com>
2024-01-14 09:16:51 +0000

    sum-of-numbers.js completed

---
+++ JS Advanced/Syntax, Functions and Statements - Lab/sum-of-numbers.js
@@ -0,0 +1,14 @@
+function sum(a, b) {
+    a = Number(a);
+    b = Number(b);
+
+    let sum = 0;
+
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+
+    console.log(sum);
+}
+
+sum('-8', '20');
