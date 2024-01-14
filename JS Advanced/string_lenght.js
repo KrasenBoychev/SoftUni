fcb42ecdb5a60b88443215bf55890fa57eb212b2
Krasen Boychev <krasenboychev11@gmail.com>
2024-01-14 08:21:51 +0000

    string_length.js completed

---
+++ JS Advanced/string_lenght.js
@@ -0,0 +1,10 @@
+function stringLength(a, b, c) {
+    
+    let sum = a.length + b.length + c.length;
+    let avg = Math.floor(sum / 3);
+
+    console.log(sum);
+    console.log(avg);
+}
+
+stringLength('chocolate', 'ice cream', 'cake')
