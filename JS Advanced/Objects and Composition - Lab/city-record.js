a4a58b84e634b782094b33aa8f4bbbc906f5f160
Krasen Boychev <krasenboychev11@gmail.com>
2024-01-20 16:00:36 +0000

    city-record.js completed

---
+++ JS Advanced/Objects and Composition - Lab/city-record.js
@@ -0,0 +1,12 @@
+function cityRecord(name, population, treasury ) {
+    const cityInfo = {
+        name,
+        population,
+        treasury
+    }
+    return cityInfo;
+}
+
+cityRecord('Tortuga',
+7000,
+15000);
