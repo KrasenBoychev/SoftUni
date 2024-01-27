0214fb3bef46181bd648055a542e1ebe2f45b2a7
Krasen Boychev <krasenboychev11@gmail.com>
2024-01-27 13:14:53 +0000

    accordion.js completed

--- JS Advanced/DOM Introduction - Exercise/03. Accordion/accordion.js
+++ JS Advanced/DOM Introduction - Exercise/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    let command = document.getElementsByClassName('button')[0].textContent;
+
+    if (command == `More`) {
+        document.getElementById('extra').style.display = 'block';
+        document.getElementsByClassName('button')[0].textContent = 'Less';
+
+    } else if (command == `Less`) {
+        document.getElementById('extra').style.display = 'none';
+        document.getElementsByClassName('button')[0].textContent = 'More';
+    }   
 }
