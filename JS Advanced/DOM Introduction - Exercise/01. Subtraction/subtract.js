ccf4d0c8eeda0d04726002297c502bf19c54baf9
Krasen Boychev <krasenboychev11@gmail.com>
2024-01-27 11:47:59 +0000

    subtract.js completed

--- JS Advanced/DOM Introduction - Exercise/01. Subtraction/subtract.js
+++ JS Advanced/DOM Introduction - Exercise/01. Subtraction/subtract.js
@@ -1,3 +1,6 @@
 function subtract() {
-    console.log('TODO:...');
+    let first = document.getElementById('firstNumber').value;
+    let second = document.getElementById('secondNumber').value;
+
+    document.getElementById('result').textContent = Number(first) - Number(second);
 }
