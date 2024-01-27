0addcc2d13802adcdbca2f1a194cd119855b88f5
Krasen Boychev <krasenboychev11@gmail.com>
2024-01-27 14:49:20 +0000

    solution.js completed

--- JS Advanced/DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ JS Advanced/DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,28 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let selectedRows = Array.from(document.getElementsByClassName("select"));
+      selectedRows.forEach(row => {
+         row.classList.remove("select");
+      });
 
+      let userInput = document.getElementById('searchField').value;
+      let studentsInfo = document.querySelectorAll('tbody tr');
+
+      for (let studentInfo of studentsInfo) {
+
+         let student = studentInfo.querySelectorAll('td');
+
+         for (let info of student) {
+
+            let content = info.textContent;
+
+            if (content.includes(userInput)) {
+               studentInfo.classList.add("select");
+            }
+         }
+
+      }
+      document.getElementById('searchField').value = '';
    }
 }
