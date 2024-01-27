c7411842f7094ce8e94dc8450440d56692456a60
Krasen Boychev <krasenboychev11@gmail.com>
2024-01-27 13:46:21 +0000

    search.js completed

--- JS Advanced/DOM Introduction - Exercise/04. Search in List/search.js
+++ JS Advanced/DOM Introduction - Exercise/04. Search in List/search.js
@@ -1,3 +1,16 @@
 function search() {
-   // TODO
+   let userInput = document.getElementById('searchText').value;
+   let towns = Array.from(document.querySelectorAll('ul#towns li'));
+   let count = 0;
+
+   for (let i = 0; i < towns.length; i++) {
+      let townName = towns[i].textContent;
+      
+      if (townName.includes(userInput)) {
+         towns[i].style.textDecoration = "underline";
+         towns[i].style.fontWeight = "bold";
+         count++;
+      }
+   }
+   document.getElementById('result').textContent = `${count} matches found`;
 }
