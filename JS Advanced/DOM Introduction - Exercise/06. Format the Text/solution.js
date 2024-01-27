a0fd24803927d3f37c5b1fc17fbb2d0ba3b9e77b
Krasen Boychev <krasenboychev11@gmail.com>
2024-01-27 16:28:51 +0000

    solution.js

--- JS Advanced/DOM Introduction - Exercise/06. Format the Text/solution.js
+++ JS Advanced/DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,3 +1,27 @@
 function solve() {
-  //TODO
+  let text = document.getElementById('input').value;
+  let splitText = text.split(`.`);
+  let output = document.getElementById('output');
+
+  let count = 1;
+  let result = ``;
+
+  for (let sentence of splitText) {
+
+    if (sentence.length > 0) {
+
+      if (count > 3) {
+        output.innerHTML += `<p> ${result} </p>`;
+        result = ``;
+        count = 1;
+      }
+
+      result += `${sentence}.`;
+      count++;
+    }
+  }
+
+  if (result.length > 0) {
+    output.innerHTML += `<p>${result}</p>`;
+  }
 }
