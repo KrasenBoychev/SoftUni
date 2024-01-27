7c0fd5e91e6624bdcffa670c3529ab9f859d0128
Krasen Boychev <krasenboychev11@gmail.com>
2024-01-27 12:19:57 +0000

    solution.js completed

--- JS Advanced/DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
+++ JS Advanced/DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,31 @@
 function solve() {
-  //TODO...
+  let input = document.getElementById('text').value;
+  let namingConvention = document.getElementById('naming-convention').value;
+
+  input = input.toLowerCase();
+  let words = input.split(` `);
+
+  let [caseType] = namingConvention.split(` `);
+  let result = ``;
+
+  if (caseType == "Camel") {
+    result = words[0];
+    for (let i = 1; i < words.length; i++) {
+      let newFirstLetter = words[i][0].toUpperCase();
+      words[i] = words[i].replace(words[i][0], newFirstLetter);
+      result += words[i];
+    }
+
+  } else if (caseType == "Pascal") {
+    for (let word of words) {
+      let newFirstLetter = word[0].toUpperCase();
+      word = word.replace(word[0], newFirstLetter);
+      result += word;
+    }
+
+  } else {
+    result = `Error!`
+  }
+
+  document.getElementById('result').textContent = result;
 }
