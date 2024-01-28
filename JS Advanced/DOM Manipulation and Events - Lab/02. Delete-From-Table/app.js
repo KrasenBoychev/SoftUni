f7bc582ed52e8eea540155a5cbebc6d0ad58b254
Krasen Boychev <krasenboychev11@gmail.com>
2024-01-28 19:41:00 +0000

    app.js

--- JS Advanced/DOM Manipulation and Events - Lab/02. Delete-From-Table/app.js
+++ JS Advanced/DOM Manipulation and Events - Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,13 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let userInput = document.querySelector('[name="email"]').value;
+    let rows = document.querySelectorAll('tbody tr');
+    let result = document.getElementById('result');
+    result.textContent = 'Not found.';
+
+    for (let row of rows) {
+        if (row.children[1].textContent == userInput) {
+            row.remove();
+            result.textContent = 'Deleted.';
+        }
+    }
 }
