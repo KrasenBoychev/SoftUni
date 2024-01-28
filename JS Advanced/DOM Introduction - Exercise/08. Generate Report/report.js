9d7294ef287f51816c52229dee5fdd883edb8834
Krasen Boychev <krasenboychev11@gmail.com>
2024-01-28 09:36:39 +0000

    report.js completed

--- JS Advanced/DOM Introduction - Exercise/08. Generate Report/report.js
+++ JS Advanced/DOM Introduction - Exercise/08. Generate Report/report.js
@@ -1,3 +1,35 @@
 function generateReport() {
-    //TODO
+
+    let inputs = document.querySelectorAll('thead tr th input[type = "checkbox"]');
+    let headings = document.querySelectorAll('thead tr th');
+    let headingsIndexes = [];
+    let result = [];
+
+    for (let i = 0; i < inputs.length; i++) {
+
+        if (inputs[i].checked == true) {
+            headingsIndexes.push(Number(i));
+        }
+    }
+
+    let allEmployess = document.querySelectorAll('tbody tr');
+
+    for (let employee of allEmployess) {
+
+        let employeeChildren = employee.children;
+        let employeeObj = {};
+
+        for (let i = 0; i < employeeChildren.length; i++) {
+
+            if (headingsIndexes.includes(i)) {
+                let keyH = headings[i].textContent.toLowerCase().trim();
+                let value = employeeChildren[i].textContent;
+                employeeObj[keyH] = value;
+            }
+        }
+        result.push(employeeObj);
+    }
+
+    let resultToString = JSON.stringify(result);
+    document.getElementById('output').value = resultToString;
 }
