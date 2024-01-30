8f7d03b37a3a46f8a1b2e6eb131a4feb8f2f473f
Krasen Boychev <krasenboychev11@gmail.com>
2024-01-30 07:11:24 +0000

    app.js (add - remove) completed

--- JS Advanced/DOM Manipulation and Events - Lab/03. Add-Delete/app.js
+++ JS Advanced/DOM Manipulation and Events - Lab/03. Add-Delete/app.js
@@ -1,3 +1,19 @@
 function addItem() {
-    //TODO...
+    let userInput = document.getElementById('newItemText').value;
+    let liElement = document.createElement("li");
+    liElement.textContent = userInput;
+
+    let deleteBtn = document.createElement('a');
+    deleteBtn.textContent = '[Delete]';
+    deleteBtn.href = '#';
+    deleteBtn.addEventListener('click', onDelete);
+    liElement.appendChild(deleteBtn);
+
+    document.getElementById('items').appendChild(liElement);
+    document.getElementById('newItemText').value = '';
+
+    function onDelete(event) {
+        let liElement = event.target.parentElement;
+        liElement.remove();
+    }
 }
