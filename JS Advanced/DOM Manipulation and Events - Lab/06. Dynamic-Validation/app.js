e0f306aab1fb6b33aa9cf9085f7486b4fce049e9
Krasen Boychev <krasenboychev11@gmail.com>
2024-01-30 09:25:56 +0000

    Dynamic Validation exercise completed

--- JS Advanced/DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
+++ JS Advanced/DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,16 @@
 function validate() {
-    console.log('TODO:...');
+    emailPattern = /^[a-z]+@[a-z]+.[a-z]+/g;
+
+    let inputField = document.getElementById('email');
+    inputField.addEventListener('change', checkEmail);
+
+    function checkEmail(event) {
+        let email = emailPattern.test(inputField.value); // another option: (event.target.value)
+        if (email) {
+           event.target.classList.remove("error");
+           return;
+        } else {
+            event.target.classList.add("error");
+        }
+    }
 }
