d6a4a72767ab7640775e69ae0e3889dedf42cf4b
Krasen Boychev <krasenboychev11@gmail.com>
2024-01-30 13:27:23 +0000

    Fill Dropdown exercise completed

--- JS Advanced/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ JS Advanced/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,15 @@
 function addItem() {
-    console.log('TODO:...');
+    const addBtn = document.querySelector('input[type="button"]');
+    const item = document.getElementById('newItemText');
+    const value = document.getElementById('newItemValue');
+
+    const option = document.createElement("option");
+    option.text = item.value;
+    option.value = value.value;
+
+    const selectElement = document.getElementById('menu');
+    selectElement.appendChild(option);
+
+    item.value = ``;
+    value.value = ``;
 }
