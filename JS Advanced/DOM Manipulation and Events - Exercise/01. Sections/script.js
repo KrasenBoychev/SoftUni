d24dcba49b1ca5560402108946fe78942eee4278
Krasen Boychev <krasenboychev11@gmail.com>
2024-01-30 11:07:09 +0000

    Sections exercise completed

--- JS Advanced/DOM Manipulation and Events - Exercise/01. Sections/script.js
+++ JS Advanced/DOM Manipulation and Events - Exercise/01. Sections/script.js
@@ -1,3 +1,22 @@
 function create(words) {
-   console.log('TODO:...');
+   const mainDiv = document.getElementById('content');
+
+      for (const word of words) {
+         const div = document.createElement('div');
+         const p = document.createElement('p');
+         div.appendChild(p);
+
+         p.textContent = word;
+         p.style.display = 'none';
+
+         div.addEventListener('click', showText);
+
+         function showText(event) {
+            const clickedDiv =  event.target;
+            const pToShow = clickedDiv.getElementsByTagName('p')[0];
+            pToShow.style.display = 'block';
+         }
+         
+         mainDiv.appendChild(div);
+      }
 }
