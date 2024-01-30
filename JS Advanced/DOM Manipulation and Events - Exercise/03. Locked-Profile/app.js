810d5f5d8da12287a9342b2faa8c6fd343459dbd
Krasen Boychev <krasenboychev11@gmail.com>
2024-01-30 12:54:50 +0000

    Locked Profile exercise completed

--- JS Advanced/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
+++ JS Advanced/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,25 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons = Array.from(document.querySelectorAll('button'));
+    
+    for (const btn of buttons) {
+        btn.addEventListener('click', showMore);
+    }
+
+    function showMore(event) {
+        const currBtn = event.currentTarget;
+        const divProfile = currBtn.parentElement;
+        const unlockInput = divProfile.children[4];
+
+        if(unlockInput.checked == true) {
+
+            if (currBtn.textContent == 'Show more') {
+                divProfile.querySelector('div').style.display = 'block';
+                currBtn.textContent = 'Hide it';
+
+            } else {
+                divProfile.querySelector('div').style.display = 'none';
+                currBtn.textContent = 'Show more';
+            }
+        }
+    }
 }
