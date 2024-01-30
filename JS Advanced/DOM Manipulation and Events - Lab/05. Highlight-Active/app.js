87cf80d47af55ef3cb475f9fe15f321a7c38b0c4
Krasen Boychev <krasenboychev11@gmail.com>
2024-01-30 08:07:52 +0000

    Highlight Active exercise completed

--- JS Advanced/DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
+++ JS Advanced/DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
@@ -1,3 +1,17 @@
 function focused() {
-    console.log('TODO:...');
+    let inputFields = Array.from(document.querySelectorAll('div input'));
+   
+    for (let inputField of inputFields) {
+        inputField.addEventListener('blur', lostFocus);
+        inputField.addEventListener('focus', changeBackground);
+    }
+
+    function changeBackground(event) {
+        event.target.parentElement.classList.add("focused");
+    }
+
+    function lostFocus(event) {
+        event.target.parentElement.classList.remove("focused");
+    }
 }
+
