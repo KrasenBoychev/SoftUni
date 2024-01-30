6e339cdd779ccb7a3a8e53bd5f4ceeadefaeb721
Krasen Boychev <krasenboychev11@gmail.com>
2024-01-30 14:15:14 +0000

    Encode and Decode Messages  exercise completed

--- JS Advanced/DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
+++ JS Advanced/DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,48 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let buttons = Array.from(document.querySelectorAll('button'));
+    let textAreas = Array.from(document.querySelectorAll('textarea'));
+    let typedMessage = textAreas[0];
+    let receivedMessage = textAreas[1];
+
+    for (const btn of buttons) {
+        btn.addEventListener('click', action);
+    }
+
+    function action(event) {
+        let actionType = event.target;
+
+        if (actionType.textContent == "Encode and send it") {
+            let typedMessageValue = typedMessage.value;
+
+            if (typedMessageValue.length > 0) {
+                let newMessage = '';
+
+                for (let symbol of typedMessageValue) {
+                    let asciiSymbol = symbol.charCodeAt() + 1;
+                    newMessage += String.fromCharCode(asciiSymbol);
+                }
+
+                receivedMessage.value = newMessage;
+                typedMessage.value = '';
+            }
+        }
+
+        if (actionType.textContent == "Decode and read it") {
+            let receivedMessageValue = receivedMessage.value;
+            let newMessage = '';
+
+            for (let symbol of receivedMessageValue) {
+                let asciiSymbol = symbol.charCodeAt() - 1;
+                newMessage += String.fromCharCode(asciiSymbol);
+            }
+
+            receivedMessage.value = newMessage;
+        }
+
+    }
 }
+
+
+
+
+// The password for my bank account is 123pass32
