780b9573f9f4806bc06d26e022dc1bfc8fe63592
Krasen Boychev <krasenboychev11@gmail.com>
2024-02-01 13:29:25 +0000

    filter-employees.js completed

--- JS Advanced/Advanced Functions - Lab/currency-format.js
+++ JS Advanced/Advanced Functions - Lab/currency-format.js
@@ -1,9 +1,9 @@
-function createFormatter(separator, symbol, symbolFirst, currentFormatter) {
+function createFormatter(separator, symbol, symbolFirst, currencyFormatter) {
   
     let formatter = function (value) {
         return currencyFormatter(separator, symbol, symbolFirst, value);
     }
-    
+
     return formatter;
 }
 
