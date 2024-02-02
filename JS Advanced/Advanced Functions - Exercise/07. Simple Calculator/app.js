867d3d3ad6172325857e6f6c32d3f0cfd94c06e0
Krasen Boychev <krasenboychev11@gmail.com>
2024-02-02 14:22:43 +0000

    app.js -> doesn't work on Judge

--- JS Advanced/Advanced Functions - Exercise/07. Simple Calculator/app.js
+++ JS Advanced/Advanced Functions - Exercise/07. Simple Calculator/app.js
@@ -1,6 +1,26 @@
 function calculator() {
-    // TODO:
+    return {
+        init(selector1, selector2, resultSelector) {
+            let num1 = document.getElementById(selector1);
+            let num2 = document.getElementById(selector2);
+            let result = document.getElementById(resultSelector);
+
+            return num1, num2, result;
+        },
+
+        add() {
+            return result.value = Number(num1.value) + Number(num2.value);
+            
+        },
+
+        subtract() {
+            return result.value = Number(num1.value) - Number(num2.value);
+        }
+    }
 }
+
+const calculate = calculator ();
+calculate.init ('#num1', '#num2', '#result');
 
 
 
