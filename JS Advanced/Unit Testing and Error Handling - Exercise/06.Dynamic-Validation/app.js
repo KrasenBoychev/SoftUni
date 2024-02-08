218f2c3b447de8427120288bf0df9e03f0d1c1b4
Krasen Boychev <krasenboychev11@gmail.com>
2024-02-08 11:02:14 +0000

    Dynamic Validation execise completed

--- JS Advanced/Unit Testing and Error Handling - Exercise/06.Dynamic-Validation/app.js
+++ JS Advanced/Unit Testing and Error Handling - Exercise/06.Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
 function validate() {
-    // TODO
+    let emailField = document.getElementById("email");
+    let emailPattern = /^[a-z]+@[a-z]+.[a-z]+$/g;
+
+    emailField.addEventListener('change', changeClass)
+
+    function changeClass() {
+        if (!emailPattern.test(emailField.value)) {
+            emailField.classList.add('error');
+        } else {
+            emailField.classList.remove('error');
+        }
+    }
 }
