895630378a815aad7b4fc41845e2b0d0044cb7fa
Krasen Boychev <krasenboychev11@gmail.com>
2024-02-15 11:36:32 +0000

    solution.js completed

--- JS Advanced/DOM Introduction - Exercise/09. Number Convertor/solution.js
+++ JS Advanced/DOM Introduction - Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,30 @@
 function solve() {
+    let numberField = document.getElementById(`input`);
+    let toField = document.getElementById(`selectMenuTo`);
+    let btn = document.querySelector('button');
+    let result = document.getElementById(`result`);
 
-    //TODO...
+    let firstOption = toField.children[0];
+    firstOption.value = 'binary';
+    firstOption.textContent = 'Binary';
+
+    let secondOption = document.createElement('option');
+    toField.appendChild(secondOption);
+    secondOption.value = 'hexadecimal';
+    secondOption.textContent = 'Hexadecimal';
+
+    btn.addEventListener('click', convert);
+
+    function convert() {
+        let num = numberField.value;
+        let toValue = toField.value;
+
+        if (toValue == 'binary') {
+            result.value = Number(num).toString(2);
+        } 
+        else {
+            result.value = Number(num).toString(16).toUpperCase();
+        }
+    }
+
 }
