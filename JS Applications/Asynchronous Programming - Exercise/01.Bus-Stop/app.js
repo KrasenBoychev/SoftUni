96cfa08ee884feab2e880169656bba852af80d55
Krasen Boychev <krasenboychev11@gmail.com>
2024-02-28 17:28:07 +0000

    bus-stop exercise completed

--- JS Applications/Asynchronous Programming - Exercise/01.Bus-Stop/app.js
+++ JS Applications/Asynchronous Programming - Exercise/01.Bus-Stop/app.js
@@ -1,4 +1,5 @@
-const list = document.getElementById('buses');
+const stopName = document.getElementById('stopName');
+const busesUl = document.getElementById('buses');
 
 function getInfo() {
     const busId = document.getElementById('stopId').value;
@@ -6,7 +7,7 @@ function getInfo() {
 
     fetch(url)
         .then(onHeaders)
-        .then(dipslayBusses)
+        .then(dipslayBuses)
         .catch(onError);
 }
 
@@ -17,14 +18,18 @@ function onHeaders(response) {
     return response.json();
 }
 
-function dipslayBusses(data) {
-    console.log(data);
+function dipslayBuses({name, buses}) {
+    busesUl.textContent = '';
+
+    stopName.textContent = name;
+    Object.entries(buses).forEach(bus => {
+        let createli = document.createElement('li');
+        createli.textContent = `Bus ${bus[0]} arrives in ${bus[1]} minutes`;
+        busesUl.appendChild(createli);
+    });
 }
 
-function createListElement({ name, busses }) {
-    
-}
-
-function onError(error) {
-    
+function onError() {
+    busesUl.textContent = '';
+    stopName.textContent = 'Error';
 }
