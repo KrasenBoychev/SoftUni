8bd351e33a51d5bb48082a14278e4874ba154578
Krasen Boychev <krasenboychev11@gmail.com>
2024-03-14 13:50:08 +0000

    live demo - lit(1) completed

---
+++ JS Applications/Client Side Rendering - Lab/Lecture - Lit/src/app.js
@@ -0,0 +1,42 @@
+import {html, render} from 'https://unpkg.com/lit-html?3.1.2/lit-html.js';
+
+let disabled = true;
+const template = (name, clock) => html`
+<h1>Hello, ${name}</h1>
+<p>This part should not update</p>
+${clock}
+<button ?disabled=${disabled}> Experimental button</button>
+`;
+
+const clock = (time, color) => html`
+<div>
+<h2 class=${color}>Dynamic clock</h2>
+<p>The time is ${time}</p>
+</div>
+`;
+
+const root = document.querySelector('main');
+let isRed = true;
+
+document.querySelector('button').addEventListener('click', () => {
+    render(template('Dynamic content'), root);
+});
+
+setInterval(update, 1000);
+
+function update() {
+    isRed = !isRed;
+
+    render(template('Lit-html', clock(new Date(), isRed ? 'red' : 'green')), root)
+}
+
+
+/* 
+function myTag(strings, ...params) {
+    console.log(strings, params);
+
+    return '';
+}
+
+myTag`Hello , ${'inserted value'}, this is a tagged template. ${'second value'}`; 
+*/
