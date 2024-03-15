ac08774028ecafa647399a66a5f5e1a8852df826
Krasen Boychev <krasenboychev11@gmail.com>
2024-03-15 15:51:38 +0000

    04. Fill Dropdown exercise completed

--- JS Applications/Client Side Rendering - Exercise/04.Fill-Dropdown/dropdown.js
+++ JS Applications/Client Side Rendering - Exercise/04.Fill-Dropdown/dropdown.js
@@ -1,3 +1,61 @@
-function addItem() {
-    console.log('TODO:...');
+import { html, render } from "./node_modules/lit-html/lit-html.js"
+
+const URL = 'http://localhost:3030/jsonstore/advanced/dropdown';
+
+const root = document.getElementById('menu');
+const submitInput = document.querySelector('input[type="submit"]');
+submitInput.addEventListener('click', addOption);
+
+const template = (items) => html`
+    ${items.map((item) => html`
+        <option value=${item._id}>
+            ${item.text}
+        </option>
+    `)}
+`
+
+loadTowns();
+
+async function loadTowns() {
+    try {
+        const response = await fetch(URL);
+        const items = await response.json();
+
+        if (!response) {
+            const err = response.json();
+            alert(err.message);
+        }
+
+        render(template(Object.values(items)), root);
+
+    } catch (err) {
+        alert(err.message);
+    }
+}
+
+async function addOption(event) {
+    event.preventDefault();
+
+    const itemInput = document.getElementById('itemText').value;
+
+    if (!itemInput) {
+        return;
+    }
+
+    try {
+        await fetch(URL, {
+            method: "post",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({text: itemInput})
+        });
+
+    } catch (err) {
+        alert(err.message);
+    }
+
+    loadTowns();
+
+    document.getElementById('itemText').value = "";
 }
