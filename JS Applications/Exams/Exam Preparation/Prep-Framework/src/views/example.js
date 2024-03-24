ba6bd681a34201ee36426fb36d08d38fd3e4adc7
Krasen Boychev <krasenboychev11@gmail.com>
2024-03-24 17:48:22 +0000

    exam-framework

---
+++ JS Applications/Exams/Exam Preparation/Prep-Framework/src/views/example.js
@@ -0,0 +1,11 @@
+import { html, render } from "../lib.js";
+
+const exampleTemplate = () => html`
+<section>
+    <p>Sample content</p>
+</section>
+`;
+
+export function showExample(ctx) {
+    render(exampleTemplate());
+}
