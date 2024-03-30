59cde10cbe9016204c6a50b7bcdbd8b88ce64e9f
Krasen Boychev <krasenboychev11@gmail.com>
2024-03-30 07:27:45 +0000

    create page completed

--- JS Applications/Exams/Cyberpunk Market/src/data/records.js
+++ JS Applications/Exams/Cyberpunk Market/src/data/records.js
@@ -2,9 +2,14 @@ import { get, post, put, del } from "./request.js"
 
 
 const endpoints = {
-    listOfRecords: '/data/cyberpunk?sortBy=_createdOn%20desc'
+    listOfRecords: '/data/cyberpunk?sortBy=_createdOn%20desc',
+    records: '/data/cyberpunk'
 }
 
 export async function getListOfRecords() {
     return await get(endpoints.listOfRecords);
+}
+
+export async function createRecord(data) {
+    return await post(endpoints.records, data);
 }
