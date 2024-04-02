0301eff92fb7579dd7b5bcbb145ad61abea11c54
Krasen Boychev <krasenboychev11@gmail.com>
2024-04-02 14:32:15 +0100

    calculateArea completed

---
+++ TypeScript/Introduction to TypeScript/dist/calculateArea.js
@@ -0,0 +1,6 @@
+"use strict";
+function rectangleArea(a, b) {
+    return a * b;
+}
+console.log(rectangleArea(5, 9));
+//# sourceMappingURL=calculateArea.js.map
---
+++ TypeScript/Introduction to TypeScript/src/calculateArea.ts
@@ -0,0 +1,8 @@
+function rectangleArea(a: number, b: number): number {
+    return a * b;
+}
+
+console.log(rectangleArea(5, 9));
+
+
+
