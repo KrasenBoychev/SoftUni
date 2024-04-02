3435453f6a18f8821b8bd17e5dc7ae4fb381b11a
Krasen Boychev <krasenboychev11@gmail.com>
2024-04-02 14:35:55 +0100

    typescript: largestNumber completed

---
+++ TypeScript/Introduction to TypeScript/dist/largestNumber.js
@@ -0,0 +1,6 @@
+"use strict";
+function largestNumber(a, b, c) {
+    return Math.max(a, b, c);
+}
+console.log(largestNumber(-3, -5, -22.5));
+//# sourceMappingURL=largestNumber.js.map
---
+++ TypeScript/Introduction to TypeScript/src/largestNumber.ts
@@ -0,0 +1,5 @@
+function largestNumber(a: number, b: number, c: number): number {
+    return Math.max(a, b, c);
+}
+
+console.log(largestNumber(5, -3, 16));
