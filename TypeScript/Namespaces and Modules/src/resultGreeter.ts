0286b865d6a95a62522176e6034d16d3cd9a25a9
Krasen Boychev <krasenboychev11@gmail.com>
2024-04-19 12:48:42 +0100

    greeter exercise completed

---
+++ TypeScript/Namespaces and Modules/src/resultGreeter.ts
@@ -0,0 +1,20 @@
+class Person<T> implements Greeter.Greeting<T> {
+    name: string;
+    age: number;
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    introduction(): string {
+        return `My name is ${this.name} and I am ${this.age} years old.`
+    }
+
+    sayGoodbye(name: T): string {
+        return `Dear ${name}, it was a pleasure meeting you!`
+    }
+}
+
+let p = new Person('Ivan Ivanov', 25);
+console.log(p.introduction());
+console.log(p.sayGoodbye('Petar Petrov'));
