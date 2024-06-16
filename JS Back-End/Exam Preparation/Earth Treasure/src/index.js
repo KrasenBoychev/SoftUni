fcbd01054e7e9a968d3c80c5ff553e03ef2b53d9
Krasen Boychev <krasenboychev11@gmail.com>
2024-06-16 10:39:48 +0100

    exam preparation 1 started

---
+++ JS Back-End/Exam Preparation/Earth Treasure/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { configDatabase } = require('./config/configDatabase');
+const { configHbs } = require('./config/configHbs');
+const { configExpress } = require('./config/configExpress');
+const { configRoutes } = require('./config/configRoutes');
+const { createToken, verifyToken } = require('./services/jwt');
+const { register } = require('./services/user');
+
+start();
+
+async function start() {
+    const app = express();
+
+    await configDatabase();
+    configHbs(app);
+    configExpress(app);
+    configRoutes(app);
+
+    app.listen(3000, () => {
+        console.log('Server strated http://localhost:3000');
+    });
+}
