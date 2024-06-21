99d74f91b3f39f6c5ed82f0ac51189512c0a0f0b
Krasen Boychev <krasenboychev11@gmail.com>
2024-06-21 13:17:04 +0100

    details page completed

--- JS Back-End/Exam Preparation/Second hand electronics/src/controllers/catalog.js
+++ JS Back-End/Exam Preparation/Second hand electronics/src/controllers/catalog.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { getAll } = require('../services/electronics');
+const { getAll, getById } = require('../services/electronics');
 
 
 const catalogRouter = Router();
@@ -8,5 +8,31 @@ catalogRouter.get('/catalog', async (req, res) => {
     const electronics = await getAll();
     res.render('catalog', { electronics });
 });
+
+catalogRouter.get('/catalog/:id', async (req, res) => {
+    const id = req.params.id;
+
+    const electronics = await getById(id);
+
+    if (!electronics) {
+        res.status(404).render('404');
+        return;
+    }
+
+    electronics.hasUser = res.locals.hasUser;
+    electronics.isAuthor = req.user?._id == electronics.author.toString();
+    electronics.hasVoted = Boolean(electronics.buyingList.find(v => v.toString() == req.user?._id));
+
+    res.render('details', { electronics });
+});
+
+// catalogRouter.get('/search', async (req, res) => {
+//     const { name, typeVolcano } = req.query;
+
+//     const volcanoes = await searchVolcanoes(name, typeVolcano);
+
+//     res.render('search', { data: { name, typeVolcano }, volcanoes });
+// });
+
 
 module.exports = { catalogRouter };
