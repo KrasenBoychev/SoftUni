745606ef7543524942756722cc4db7b1aa18b741
Krasen Boychev <krasenboychev11@gmail.com>
2024-06-23 10:44:32 +0100

    Exam - skeleton and basic check done

---
+++ JS Back-End/Exam Preparation/Exam - 23.06.24/src/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const { configDatabase } = require('./config/configDatabase');
+const { configHbs } = require('./config/configHbs');
+const { configExpress } = require('./config/configExpress');
+const { configRoutes } = require('./config/configRoutes');
+const { createToken, verifyToken } = require('./services/jwt');
+const { register, login } = require('./services/user');
+
+start();
+
+async function start() {
+    const app = express();
+
+    await configDatabase();
+    configHbs(app);
+    configExpress(app);
+    configRoutes(app);
+
+    app.listen(3000, () => {
+        console.log('Server strated http://localhost:3000');
+        // test();
+    });
+}
+
+async function test() {
+    try {
+
+    //This code creates a token and saves it in a cookie
+    // const result = await login('krasen', '123');
+    // const token = createToken(result);
+    // res.cookie('token', token);
+    
+    // console.log(result); 
+
+    } catch(err) {
+        console.log('Caught error');
+        console.log(err.message);
+    }
+}
